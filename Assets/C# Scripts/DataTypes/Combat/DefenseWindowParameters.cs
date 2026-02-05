381ae4888919573b40f1d6f27a4c173589c9376f
FirePixel8422 <bmtminecraft@hotmail.com>
2026-02-05 13:40:21 +0100

    Working on ParrySystems

---
+++ Assets/C# Scripts/DataTypes/Combat/DefenseWindowParameters.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+[System.Serializable]
+public struct DefenseWindowParameters
+{
+    public float Block;
+    public float Parry;
+    public float PerfectParry;
+
+    public DefenseWindowParameters(float blockWindow, float parryWindow, float perfectParryWindow)
+    {
+        Block = blockWindow;
+        Parry = parryWindow;
+        PerfectParry = perfectParryWindow;
+    }
+}
--- Assets/C# Scripts/DataTypes/Combat/SkillData/SkillStats.cs
+++ Assets/C# Scripts/DataTypes/Combat/SkillData/SkillStats.cs
@@ -5,11 +5,11 @@
 public struct SkillStats
 {
     public float damage;
-    public ParryWindowParameters parryParamaters;
+    public DefenseWindowParameters defenseWindows;
 
     public static SkillStats Default => new SkillStats()
     {
         damage = 10,
-        parryParamaters = new ParryWindowParameters(0.3f, 0.1f),
+        defenseWindows = new DefenseWindowParameters(0.3f, 0.15f, 0.05f),
     };
 }
